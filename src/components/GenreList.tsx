31966526577382f650e9ebeeb035802facfc993d
Alper Sozuoz <alper1900@hotmail.co.uk>
2024-01-23 22:59:15 +0000

    Added filtering with genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,9 +1,13 @@
-import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
-import useGenres from "../hooks/useGenres";
+import { Button, HStack, Image, List, ListItem } from "@chakra-ui/react";
+import useGenres, { Genre } from "../hooks/useGenres";
 import getCroppedImageUrl from "../services/image-url";
 import GenreListSkeleton from "./GenreListSkeleton";
 
-const GenreList = () => {
+interface GenreListProps {
+  onSelectGenre: (genre: Genre) => void;
+}
+
+const GenreList = ({ onSelectGenre } : GenreListProps) => {
   const { data, isLoading, error } = useGenres();
   const skeletons: number[] = Array.from(Array(14).keys());
   if (error) return null;
@@ -22,7 +26,13 @@ const GenreList = () => {
                 src={getCroppedImageUrl(genre.image_background)}
               />
 
-              <Text fontSize="lg">{genre.name}</Text>
+              <Button
+                onClick={() => onSelectGenre(genre)}
+                variant={"link"}
+                fontSize="lg"
+              >
+                {genre.name}
+              </Button>
             </HStack>
           </ListItem>
         ))}
