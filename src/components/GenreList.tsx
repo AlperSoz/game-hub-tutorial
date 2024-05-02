77871771a815935329b9fa943125b2524a450ac4
Alper Sozuoz <alper1900@hotmail.co.uk>
2024-05-02 18:29:48 +0100

    Simplify game query object

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,14 +1,24 @@
-import { Button, HStack, Heading, Image, List, ListItem } from "@chakra-ui/react";
+import {
+  Button,
+  HStack,
+  Heading,
+  Image,
+  List,
+  ListItem,
+} from "@chakra-ui/react";
 import useGenres, { Genre } from "../hooks/useGenres";
 import getCroppedImageUrl from "../services/image-url";
 import GenreListSkeleton from "./GenreListSkeleton";
 
 interface GenreListProps {
   onSelectGenre: (genre: Genre) => void;
-  selectedGenre: Genre | null;
+  selectedGenreId?: number;
 }
 
-const GenreList = ({ selectedGenre, onSelectGenre } : GenreListProps) => {
+const GenreList = ({
+  selectedGenreId,
+  onSelectGenre,
+}: GenreListProps) => {
   const { data, isLoading, error } = useGenres();
   const skeletons: number[] = Array.from(Array(15).keys());
   if (error) return null;
@@ -17,7 +27,9 @@ const GenreList = ({ selectedGenre, onSelectGenre } : GenreListProps) => {
     <>
       {isLoading &&
         skeletons.map((skeleton) => <GenreListSkeleton key={skeleton} />)}
-        <Heading fontSize='2xl' marginBottom={3}>Genres</Heading>
+      <Heading fontSize="2xl" marginBottom={3}>
+        Genres
+      </Heading>
       <List>
         {data?.results.map((genre) => (
           <ListItem key={genre.id} paddingY={"5px"}>
@@ -25,11 +37,14 @@ const GenreList = ({ selectedGenre, onSelectGenre } : GenreListProps) => {
               <Image
                 boxSize="32px"
                 borderRadius={8}
-                objectFit='cover'
+                objectFit="cover"
                 src={getCroppedImageUrl(genre.image_background)}
               />
 
-              <Button whiteSpace='normal' textAlign='left' fontWeight={genre.id === selectedGenre?.id ? 'bold' : 'normal' }
+              <Button
+                whiteSpace="normal"
+                textAlign="left"
+                fontWeight={genre.id === selectedGenreId ? "bold" : "normal"}
                 onClick={() => onSelectGenre(genre)}
                 variant={"link"}
                 fontSize="lg"
--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -2,22 +2,36 @@ import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
 import { BsChevronDown } from "react-icons/bs";
 import usePlatforms, { Platform } from "../hooks/usePlatforms";
 
-
 interface PlatformSelectorProps {
-    onSelectPlatform: (platform: Platform) => void
-    selectedPlatform: Platform | null;
+  onSelectPlatform: (platform: Platform) => void;
+  selectedPlatformId?: number;
 }
 
-const PlatformSelector = ({ onSelectPlatform, selectedPlatform }: PlatformSelectorProps) => {
-  const {data, error} = usePlatforms();
+const PlatformSelector = ({
+  onSelectPlatform,
+  selectedPlatformId,
+}: PlatformSelectorProps) => {
+  const { data, error } = usePlatforms();
+  const selectedPlatform = data.results.find(
+    (p) => p.id === selectedPlatformId
+  );
 
   if (error) return null;
 
   return (
     <Menu>
-      <MenuButton as={Button} rightIcon={<BsChevronDown />}>{selectedPlatform?.name || 'Platforms'}</MenuButton>
+      <MenuButton as={Button} rightIcon={<BsChevronDown />}>
+        {selectedPlatform?.name || "Platforms"}
+      </MenuButton>
       <MenuList>
-        {data?.results.map(platform => <MenuItem onClick={() => onSelectPlatform(platform)} key={platform.id}>{platform.name}</MenuItem>)}
+        {data?.results.map((platform) => (
+          <MenuItem
+            onClick={() => onSelectPlatform(platform)}
+            key={platform.id}
+          >
+            {platform.name}
+          </MenuItem>
+        ))}
       </MenuList>
     </Menu>
   );
