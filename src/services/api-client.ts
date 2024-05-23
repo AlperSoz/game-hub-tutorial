479dc811d1a91a1a36a99ac85d9e13ef4a3b5fb4
Alper Sozuoz <alper1900@hotmail.co.uk>
2024-05-23 20:30:41 +0100

    Implement fetching game details

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,30 +1,36 @@
 import axios, { AxiosRequestConfig } from "axios";
 
 export interface FetchResponse<T> {
-    count: number;
-    results: T[];
-    next: string | null;
-  }
+  count: number;
+  results: T[];
+  next: string | null;
+}
 
 const axiosInstance = axios.create({
-    baseURL: 'https://api.rawg.io/api',
-    params: {
-        key: '2116922a8c554952880b2eab52d212c0'
-    }
-})
+  baseURL: "https://api.rawg.io/api",
+  params: {
+    key: "2116922a8c554952880b2eab52d212c0",
+  },
+});
 
 class APIClient<T> {
-    endpoint: string;
+  endpoint: string;
 
-    constructor(endpoint: string) {
-        this.endpoint = endpoint;
-    }
-    
-    getAll = (config: AxiosRequestConfig) => {
-        return axiosInstance
-        .get<FetchResponse<T>>(this.endpoint, config)
-        .then(res => res.data)
-    }
+  constructor(endpoint: string) {
+    this.endpoint = endpoint;
+  }
+
+  getAll = (config: AxiosRequestConfig) => {
+    return axiosInstance
+      .get<FetchResponse<T>>(this.endpoint, config)
+      .then((res) => res.data);
+  };
+
+  get = (id: string | number) => {
+    return axiosInstance
+      .get<T>(this.endpoint + "/" + id)
+      .then((res) => res.data);
+  };
 }
 
 export default APIClient;
